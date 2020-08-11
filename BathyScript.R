8bbb82eb8db2adbde4f1c6012370fef41966009b
sam <samt9892@hotmail.com>
2020-08-11 15:37:17 +0800

    Trying to loop .csv formatting through all files - not working

--- BathyScript.R
+++ BathyScript.R
@@ -1,7 +1,22 @@
 #Script for fixing 2018bathymetry .csv files up..
 
+setwd("G:/My Drive/GIS/GIS_datasets/Bathymetry/50mMultibeamDatasetofAustralia2018")
 
-Current <- as.data.frame((read.table(filepath<-file.choose())))
+
+#trying a loop
+
+a <- (list.files(  pattern = ".csv"))
+for (i in a){
+  b <- as.data.frame(read.table(i))
+  filename <- basename(a)
+  colnames(b) <- c("Easting","Northing","Elevation")
+  write.csv(b, row.names=F, file = paste(filename, ".csv", sep = ""))
+}
+
+
+# individual files = works
+
+'Current <- as.data.frame((read.table(filepath<-file.choose())))
 colnames(Current) <- c("Easting","Northing","Elevation")
 
-write.csv(Current, "SC54v2.csv",row.names=F)
+write.csv(Current, "SC54v2.csv",row.names=F)'
