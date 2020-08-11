a5d6f714abe7a8794aeccf7516c3d567f9ea616a
sam <samt9892@hotmail.com>
2020-08-11 14:30:12 +0800

    Removed index column

--- BathyScript.R
+++ BathyScript.R
@@ -1,1 +1,7 @@
 #Script for fixing 2018bathymetry .csv files up..
+
+
+Current <- as.data.frame((read.table(filepath<-file.choose())))
+colnames(Current) <- c("Easting","Northing","Elevation")
+
+write.csv(Current, "SC54v2.csv",row.names=F)
